74613f43f78a304f117e6cf2b999f4a6711151d1
Andrei Faber <andrei.faber@gmail.com>
2016-01-27 13:39:09 +0700

    added test application for integration tests

---
+++ src/TestApp/ServiceWorker.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using GainCapital.AutoUpdate;
+using Topshelf;
+
+namespace TestApp
+{
+	class ServiceWorker : ServiceControl
+	{
+		public bool Start(HostControl hostControl)
+		{
+			_host = hostControl;
+			_updater = new UpdateChecker(_host,
+				new UpdatingInfo
+				{
+					NugetAppName = Program.AppName,
+					ServiceName = Program.AppName,
+				});
+			_updater.Start();
+
+			return true;
+		}
+
+		public bool Stop(HostControl hostControl)
+		{
+			_updater.Stop();
+
+			_host = null;
+			_updater = null;
+
+			return true;
+		}
+
+		private HostControl _host;
+		private UpdateChecker _updater;
+	}
+}
