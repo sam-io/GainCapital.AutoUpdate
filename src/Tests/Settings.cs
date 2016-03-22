2b7c6729056a94212bb644b104caaf549b3dd47c
Andrei Faber <andrei.faber@gmail.com>
2016-03-22 14:17:05 +0700

    added settings

---
+++ src/Tests/Settings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GainCapital.AutoUpdate.Tests
+{
+	static class Settings
+	{
+		public static string NugetUrl = "http://localhost:49361/nuget/";
+	}
+}
