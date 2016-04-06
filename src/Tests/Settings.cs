552429a3da9aec5c5041c123a6a70bd17959cea6
Andrei Faber <andrei.faber@gmail.com>
2016-04-06 16:55:34 +0700

    Klondike - force interactive mode

--- src/Tests/Settings.cs
+++ src/Tests/Settings.cs
@@ -9,7 +9,7 @@ namespace GainCapital.AutoUpdate.Tests
 	static class Settings
 	{
 		public const string KlondikeUrl = "https://github.com/themotleyfool/Klondike/releases/download/v2.0.0/Klondike.2.0.0.26c3df25-build144.zip";
-		public const string KlondikeStarArgs = "--port=49361";
+		public const string KlondikeStarArgs = "--port=49361 --interactive";
 
 		public const string NugetUrl = "http://localhost:49361/api/";
 		public const string UpdatePackageLevel = "Beta";
